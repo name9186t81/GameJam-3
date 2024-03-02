f17f434c6f57e4add5c4de178ff58c5a4372a682
XMAKEDTF <68771834+XMAKEDTF@users.noreply.github.com>
2024-03-02 23:11:20 +0300

    улучшения физики (еда где актор и инпут)

--- Assets/Scripts/Slime/BoneJointsConnector.cs
+++ Assets/Scripts/Slime/BoneJointsConnector.cs
@@ -1,46 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class BoneJointsConnector : MonoBehaviour
 {
-    [SerializeField] private float JointDamper = 0;
-    [SerializeField] private float JointFrequency = 3;
+    [SerializeField] private GenerationMethod generationMethod;
+    [SerializeField] private SpringJointSettings EdgeJoint;
+    [SerializeField] private SpringJointSettings StructuralJoint;
     [SerializeField] private float ColliderRadius = 0.15f;
     [SerializeField] private float ColliderOffset = 0.0f;
+
+    public enum GenerationMethod
+    {
+        NeighboursAndOpposite,
+        AllToAll //попробовал для теста, говно
+    }
+
+    [System.Serializable] //хотел делать структурой но она не поддерживает значения по умолчанию и пустые конструкторы (говно)
+    private class SpringJointSettings
+    {
+        [Range(0.0f, 1.0f)]
+        public float JointDamper = 0;
+        public float JointFrequency = 3;
+    }
+
+    [InspectorButton(nameof(generateRigidbodies))]
+    [SerializeField] private bool GenerateRigidbodies;
+
+    private void generateRigidbodies()
+    {
+        var childs = GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < childs.Length; i++)
+            if(childs[i] != transform && !childs[i].TryGetComponent(out Rigidbody2D b))
+                childs[i].gameObject.AddComponent<Rigidbody2D>();
+    }
+
+    private void OnValidate()
+    {
+        if(!TryGetComponent(out SpriteSkin skin))
+        {
+            //удаляем трансформы которые насрал SpriteSkin при его пересоздании
+            var childs = GetComponentsInChildren<Transform>();
+
+            foreach (var child in childs)
+                if (child != transform)
+                    Destroy(child.gameObject);
+        }
+    }
 
     private void Awake()
     {
         var childs = GetComponentsInChildren<Rigidbody2D>();
 
-        var half = childs.Length / 2;
-
-        for (int i = 0; i < childs.Length; i++)
+        switch (generationMethod)
         {
-            var current = childs[i];
-            var next = childs[LoopIndex(i + 1, childs.Length)];
-            var previous = childs[LoopIndex(i - 1, childs.Length)];
-            var opposite = childs[LoopIndex(i + half, childs.Length)];
+            case GenerationMethod.NeighboursAndOpposite:
+                {
+                    var half = childs.Length / 2;
 
-            Link(current, next);
-            Link(current, previous);
-            Link(current, opposite);
+                    for (int i = 0; i < childs.Length; i++)
+                    {
+                        var current = childs[i];
+                        var next = childs[LoopIndex(i + 1, childs.Length)];
+                        var previous = childs[LoopIndex(i - 1, childs.Length)];
+                        var opposite = childs[LoopIndex(i + half, childs.Length)];
 
-            var col = current.gameObject.AddComponent<CircleCollider2D>();
-            col.offset = Vector2.right * ColliderOffset;
-            col.radius = ColliderRadius;
+                        Link(current, next, EdgeJoint);
+                        Link(current, previous, EdgeJoint);
+                        Link(current, opposite, StructuralJoint);
+
+                        AddBaseComponents(current);
+                    }
+                    break;
+                }
+            case GenerationMethod.AllToAll:
+                {
+                    for (int i = 0; i < childs.Length; i++)
+                    {
+                        var current = childs[i];
+                        AddBaseComponents(current);
+
+                        for (int j = 0; j < childs.Length; j++)
+                        {
+                            Link(current, childs[j], StructuralJoint);
+                        }
+                    }
+                    break;
+                }
         }
-
-        Physics2D.SyncTransforms();
     }
 
-    private void Link(Rigidbody2D a, Rigidbody2D b)
+    private void AddBaseComponents(Rigidbody2D obj)
+    {
+        var col = obj.gameObject.AddComponent<CircleCollider2D>();
+        col.offset = Vector2.right * ColliderOffset;
+        col.radius = ColliderRadius;
+    }
+
+    private void Link(Rigidbody2D a, Rigidbody2D b, SpringJointSettings s)
     {
         var joint = a.gameObject.AddComponent<SpringJoint2D>();
         joint.connectedBody = b;
 
-        joint.dampingRatio = JointDamper;
-        joint.frequency = JointFrequency;
+        joint.dampingRatio = s.JointDamper;
+        joint.frequency = s.JointFrequency;
 
         //думаю что что-то из этого можно убрать но мне лень
         joint.autoConfigureConnectedAnchor = true;
