d9a766b06957d1bfeb18190dff07a4191dd12bfc
XMAKEDTF <68771834+XMAKEDTF@users.noreply.github.com>
2024-03-04 19:06:27 +0300

    меню паузы и юи префабы

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InGameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Pause menu")]
+    [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private Text _time;
+    [SerializeField] private Slider _volumeSlider;
+
+    private bool pause = false;
+    private float startTime;
+
+    private void Awake()
     {
-        
+        startTime = Time.time;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause = !pause;
+            if(pause)
+                OpenPauseMenu();
+            else
+                ClosePauseMenu();
+        }
+    }
+
+    public void OnRestartButton()
+    {
+        ClosePauseMenu();
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnMenuButton()
+    {
+        ClosePauseMenu();
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnResumeButton()
+    {
+        ClosePauseMenu();
+    }
+
+    public void OnVolumeSliderValueChanged(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    private void OpenPauseMenu()
+    {
+        _pausePanel.SetActive(true);
+        Time.timeScale = 0f; //пока так
+        _volumeSlider.value = AudioListener.volume;
+        _time.text = "Time: " + (new TimeSpan(10000L * 1000L * (long)(Time.time - startTime)).ToString()); //da
+    }
+
+    private void ClosePauseMenu()
+    {
+        _pausePanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
