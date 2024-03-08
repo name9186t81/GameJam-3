c3b277ceadacc340410bf84237f91a7fe873ffa2
FRand <gvp3464@gmail.com>
2024-03-08 12:44:03 +0300

    Сервис локатор спидран

--- Assets/Scripts/AI/States/TouchAttack.cs
+++ Assets/Scripts/AI/States/TouchAttack.cs
@@ -1,8 +1,34 @@
-using UnityEngine;
-
-namespace Global
+namespace AI.States
 {
-	public class TouchAttack
+	 //не юзать нигде кроме классов которые наносят урон по коллизии!!!
+	public sealed class TouchAttack : IUtility
 	{
+		private AIController _controller;
+
+		public void Execute()
+		{
+			if (_controller.IsTargetNull) return;
+
+			_controller.MoveToPoint(_controller.CurrentTarget.Position);
+		}
+
+		public float GetEffectivness()
+		{
+			return _controller.IsTargetNull ? -1f : 1f;
+		}
+
+		public void Init(AIController controller)
+		{
+			_controller = controller;
+		}
+
+		public void PreExecute()
+		{
+		}
+
+		public void Undo()
+		{
+			_controller.StopMoving();
+		}
 	}
 }
---
+++ Assets/Scripts/Core/GlobalDeathNotificator.cs
@@ -0,0 +1,15 @@
+﻿using Core;
+
+using Health;
+
+using System;
+
+public sealed class GlobalDeathNotificator : IService
+{
+	public event Action<DamageArgs, IActor> OnDeath;
+
+	public void Die(DamageArgs args, IActor actor)
+	{
+		OnDeath?.Invoke(args, actor);
+	}
+}
