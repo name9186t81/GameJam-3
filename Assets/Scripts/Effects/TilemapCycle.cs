7825d20394ecb18a244d517ede67fb132c7aa27c
XMAKEDTF <68771834+XMAKEDTF@users.noreply.github.com>
2024-03-19 11:51:15 +0300

    коллайдеры и смена дня и ночи изменены

--- Assets/Scripts/Effects/TilemapCycle.cs
+++ Assets/Scripts/Effects/TilemapCycle.cs
@@ -27,6 +27,7 @@ namespace Effects
 			_shadowAngleID = Shader.PropertyToID("_Angle");
 			_shadowLengthID = Shader.PropertyToID("_Length");
 			_affinityID = Shader.PropertyToID("_Affinity");
+			_elapsed += _cycleTime / 2;
 		}
 		private void Update()
 		{
@@ -39,11 +40,13 @@ namespace Effects
 				delta -= 1;
 			}
 
+			var repeatDelta = 1 - Mathf.Abs(1 - (delta * 2));
+
 			float angle = delta * Mathf.PI * 2;
 			float length = _shadowGrowthCurve.Evaluate(delta) * _maxShadowLength;
 			float affinity = _affinityGrowthCurve.Evaluate(delta) * _maxAffinity;
-			Color mapColor = _tileMapColor.Evaluate(delta);
-			Color shadowColor = _shadowColor.Evaluate(delta);
+			Color mapColor = _tileMapColor.Evaluate(repeatDelta);
+			Color shadowColor = _shadowColor.Evaluate(repeatDelta);
 
 			_material.SetColor(_mapColorID, mapColor);
 			_material.SetColor(_shadowColorID, shadowColor);
